c51bb0db7c6de98abe18b6c921825132eed819b6
Derek-Leung <d.leung313@hotmail.com>
2020-02-02 15:37:03 -0800

    added lab 3

---
+++ lab3.js
@@ -0,0 +1,71 @@
+let artistID = 0;
+
+let addArtist = () => {
+  let artistNameInput = document.getElementById("artistName");
+  let artistName = artistNameInput.value;
+  let aboutArtistInput = document.getElementById("aboutArtist");
+  let aboutArtist = aboutArtistInput.value;
+  let imageURLInput = document.getElementById("imageURL");
+  let imageURL = imageURLInput.value;
+
+  artistNameInput.value = "";
+  aboutArtistInput.value = "";
+  imageURLInput.value = "";
+
+  let img = createArtistImage(imageURL);
+  let descriptionDiv = createArtistDescriptionDiv(artistName, aboutArtist);
+  let li = document.createElement("li");
+  let deleteBtn = createDeleteBtn(artistID);
+
+  li.appendChild(img);
+  li.appendChild(descriptionDiv);
+  li.appendChild(deleteBtn);
+
+  li.id = artistID++;
+
+  document.getElementById("list").appendChild(li);
+  showAddArtist(false);
+};
+
+let showAddArtist = show => {
+  let addForm = document.getElementById("addArtist");
+  if (show) addForm.style.display = "inline";
+  else addForm.style.display = "none";
+};
+
+let createArtistImage = imageURL => {
+  let img = document.createElement("img");
+
+  img.src = imageURL;
+  img.className = "artistImage";
+  img.alt = "Artist Image";
+  return img;
+};
+
+let createArtistDescriptionDiv = (artistName, aboutArtist) => {
+  let descriptionDiv = document.createElement("div");
+  let artistNameHeader = document.createElement("h3");
+  let artistDescription = document.createElement("p");
+
+  artistNameHeader.textContent = artistName;
+  artistDescription.textContent = aboutArtist;
+
+  descriptionDiv.className = "artistDescription";
+  descriptionDiv.appendChild(artistNameHeader);
+  descriptionDiv.appendChild(artistDescription);
+
+  return descriptionDiv;
+};
+
+let createDeleteBtn = artistID => {
+  let deleteBtn = document.createElement("button");
+  deleteBtn.className = "artistDeleteBtn";
+  deleteBtn.innerText = "Delete";
+
+  deleteBtn.onclick = () => {
+    let element = document.getElementById(artistID);
+    element.parentNode.removeChild(element);
+  };
+
+  return deleteBtn;
+};
